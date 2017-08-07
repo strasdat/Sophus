9e5a01a412961e43b29283e6d6ade4712f96ceba
Hauke Strasdat <hauke.strasdat@oculus.com>
2017-08-07 16:12:32 -0700

    foo

--- scripts/install_osx_deps.sh
+++ scripts/install_osx_deps.sh
@@ -5,11 +5,16 @@ set -e # exit on error
 brew update
 brew install glog
 brew install suite-sparse
+git clone https://github.com/RLovelett/eigen.git eigen3
+git reset --hard 80d4cce2e3a06b137a93a5179eb8b0d6bc526ac0
+cd eigen3
+mkdir build
+cmake ..
+cd ../..
 wget http://ceres-solver.org/ceres-solver-1.12.0.tar.gz
 tar zxf ceres-solver-1.12.0.tar.gz
 mkdir ceres-bin
 cd ceres-bin
 cmake -DCXX11=On ../ceres-solver-1.12.0
 make -j3
-make test
 make install
