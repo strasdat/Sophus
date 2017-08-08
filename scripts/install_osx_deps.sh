67fcfb347dec15fb96b95355faeab14c46bddd93
Hauke Strasdat <hauke.strasdat@oculus.com>
2017-08-07 17:07:21 -0700

    stuff

--- scripts/install_osx_deps.sh
+++ scripts/install_osx_deps.sh
@@ -7,7 +7,6 @@ brew install glog
 brew install suite-sparse
 git clone https://github.com/RLovelett/eigen.git eigen3
 cd eigen3
-git reset --hard 80d4cce2e3a06b137a93a5179eb8b0d6bc526ac0
 mkdir build
 cmake ..
 cd ../..
