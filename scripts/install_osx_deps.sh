a76041ad5327b0ec7237d3b2217bc5a98972c328
Hauke Strasdat <hauke.strasdat@oculus.com>
2017-08-07 20:37:38 -0700

    ccache

--- scripts/install_osx_deps.sh
+++ scripts/install_osx_deps.sh
@@ -6,11 +6,16 @@ brew update
 brew install eigen
 brew install glog
 brew install suite-sparse
+brew install ccache
+export PATH="/usr/local/opt/ccache/libexec:$PATH"
+whereis ccache
 git clone https://ceres-solver.googlesource.com/ceres-solver ceres-solver
 cd ceres-solver
 git reset --hard afe93546b67cee0ad205fe8044325646ed5deea9
 mkdir build
 cd build
-cmake -DCXX11=On ..
+ccache -M 50G
+ccache -s
+cmake -DCXX11=On -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
 make -j3
 make install
