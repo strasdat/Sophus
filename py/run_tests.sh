ab7cac5a29c589ad31a2f9f8b43f8e824ef5f871
Hauke Strasdat <strasdat@gmail.com>
2018-01-10 01:28:21 -0800

    sympy so2, se2

--- py/run_tests.sh
+++ py/run_tests.sh
@@ -3,5 +3,7 @@
 python3 -m sophus.complex
 python3 -m sophus.quaternion
 python3 -m sophus.dual_quaternion
+python3 -m sophus.so2
+python3 -m sophus.se2
 python3 -m sophus.so3
 python3 -m sophus.se3
