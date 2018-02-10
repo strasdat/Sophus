52f1748af065d25f6997011cc6f6e8eb3308514d
Hauke Strasdat <strasdat@gmail.com>
2018-02-10 15:21:20 -0800

    LCOV_EXCL_END -> LCOV_EXCL_STOP

--- sophus/rxso2.hpp
+++ sophus/rxso2.hpp
@@ -547,7 +547,7 @@ class Map<Sophus::RxSO2<Scalar_>, Options>
 
   // LCOV_EXCL_START
   EIGEN_INHERIT_ASSIGNMENT_EQUAL_OPERATOR(Map)
-  // LCOV_EXCL_END
+  // LCOV_EXCL_STOP
   using Base::operator*=;
   using Base::operator*;
 
--- sophus/se3.hpp
+++ sophus/se3.hpp
@@ -939,7 +939,7 @@ class Map<Sophus::SE3<Scalar_>, Options>
 
   // LCOV_EXCL_START
   EIGEN_INHERIT_ASSIGNMENT_EQUAL_OPERATOR(Map)
-  // LCOV_EXCL_END
+  // LCOV_EXCL_STOP
 
   using Base::operator*=;
   using Base::operator*;
