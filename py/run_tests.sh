e7dfbc163253426d5f486ccf4282ab7476710350
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2018-11-29 21:27:32 +0100

    fail python tests if any test fails (else ci passes always)

--- py/run_tests.sh
+++ py/run_tests.sh
@@ -1,9 +1,13 @@
-#bin/bash
+#!/bin/bash
 
-python3 -m sophus.complex
-python3 -m sophus.quaternion
-python3 -m sophus.dual_quaternion
-python3 -m sophus.so2
-python3 -m sophus.se2
-python3 -m sophus.so3
-python3 -m sophus.se3
+EXIT=0
+
+python3 -m sophus.complex || EXIT=$?
+python3 -m sophus.quaternion || EXIT=$?
+python3 -m sophus.dual_quaternion || EXIT=$?
+python3 -m sophus.so2 || EXIT=$?
+python3 -m sophus.se2 || EXIT=$?
+python3 -m sophus.so3 || EXIT=$?
+python3 -m sophus.se3 || EXIT=$?
+
+exit $EXIT
