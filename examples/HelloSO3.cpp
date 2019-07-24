cac8ae420d6d851490141d6f27b1f979c4fba1e2
Pieter <pieter.vangoor@anu.edu.au>
2019-07-24 14:00:56 +1000

    Adjust formatting and correct unit quaternions bit

--- examples/HelloSO3.cpp
+++ examples/HelloSO3.cpp
@@ -1,40 +1,40 @@
-#include "sophus/geometry.hpp"
 #include <iostream>
+#include "sophus/geometry.hpp"
 
 int main() {
+  // The following demonstrates the group multiplication of rotation matrices
 
-    // The following demonstrates the group multiplication of rotation matrices
+  // Create rotation matrices from rotations around the x and y and z axes:
+  const double kPi = Sophus::Constants<double>::pi();
+  Sophus::SO3d R1 = Sophus::SO3d::rotX(kPi / 4);
+  Sophus::SO3d R2 = Sophus::SO3d::rotY(kPi / 6);
+  Sophus::SO3d R3 = Sophus::SO3d::rotZ(-kPi / 3);
 
-    // Create rotation matrices from rotations around the x and y and z axes:
-    Sophus::SO3d R1 = Sophus::SO3d::rotX(M_PI/4);
-    Sophus::SO3d R2 = Sophus::SO3d::rotY(M_PI/6);
-    Sophus::SO3d R3 = Sophus::SO3d::rotZ(-M_PI/3);
+  std::cout << "The rotation matrices are" << std::endl;
+  std::cout << "R1:\n" << R1.matrix() << std::endl;
+  std::cout << "R2:\n" << R2.matrix() << std::endl;
+  std::cout << "R3:\n" << R3.matrix() << std::endl;
+  std::cout << "Their product R1*R2*R3:\n"
+            << (R1 * R2 * R3).matrix() << std::endl;
+  std::cout << std::endl;
 
-    std::cout << "The rotation matrices are" << std::endl;
-    std::cout << "R1:\n" << R1.matrix() << std::endl;
-    std::cout << "R2:\n" << R2.matrix() << std::endl;
-    std::cout << "R3:\n" << R3.matrix() << std::endl;
-    std::cout << "Their product R1*R2*R3:\n" << (R1*R2*R3).matrix() << std::endl;
+  // Creating vectors is the same as in Eigen
+  Eigen::Vector3d x;
+  x << 0.0, 0.0, 1.0;
 
-    // Creating vectors is the same as in Eigen
-    Sophus::Vector3d x;
-    x << 0.0, 0.0, 1.0;
+  // Rotation matrices can act on vectors
+  std::cout << "Rotation matrices can act on vectors" << std::endl;
+  std::cout << "x\n" << x << std::endl;
+  std::cout << "R2*x\n" << R2 * x << std::endl;
+  std::cout << "R1*(R2*x)\n" << R1 * (R2 * x) << std::endl;
+  std::cout << "(R1*R2)*x\n" << (R1 * R2) * x << std::endl;
+  std::cout << std::endl;
 
-    // Rotation matrices can act on vectors
-    std::cout << std::endl;
-    std::cout << "Rotation matrices can act on vectors" << std::endl;
-    std::cout << "x\n" << x << std::endl;
-    std::cout << "R2*x\n" << R2*x << std::endl;
-    std::cout << "R1*(R2*x)\n" << R1*(R2*x) << std::endl;
-    std::cout << "(R1*R2)*x\n" << (R1*R2)*x << std::endl;
-
-    // You can also use SO(3) objects as unit quaternions.
-    auto q1 = R1.unit_quaternion();
-    auto q2 = R2.unit_quaternion();
-    auto q3 = R3.unit_quaternion();
-    auto q1xq2xq3 = q1*q2*q3;
-    Sophus::SO3d R4 = Sophus::SO3d(q1xq2xq3);
-
-    std::cout << "Using unit quaternions, the product R1*R2*R3 is:\n" << R4.matrix() << std::endl;
-    
+  // SO(3) are internally represented as unit quaternions.
+  std::cout << "R1 in matrix form:\n" << R1.matrix() << std::endl;
+  std::cout << "R1 in unit quaternion form:\n"
+            << R1.unit_quaternion().coeffs() << std::endl;
+  // Note that the order of coefficiences of Eigen's quaternion class is (imag0,
+  // imag1, imag2, real)
+  std::cout << std::endl;
 }
