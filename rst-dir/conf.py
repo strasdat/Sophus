4da9a55431ea4cd48ec8a68ad996a2801e288e6a
Hauke Strasdat <strasdat@gmail.com>
2021-05-08 19:33:13 -0700

    py -> sympy (#293)

--- rst-dir/conf.py
+++ rst-dir/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../py'))
+sys.path.insert(0, os.path.abspath('../sympy'))
 
 
 sys.path.insert(1, os.path.abspath('../doxyrest_b/doxyrest/sphinx'))
