strasdat/Sophus

commit 8d52cd1317b8b175a42141bd50ced181e28089a1

make libfmt dependency optional (#300)

sophus/common.hpp
@@ -7,13 +7,21 @@
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
-#include <iostream>
 #include <random>
 #include <type_traits>
 
 #include <Eigen/Core>
 
 #undef SOPHUS_COMPILE_TIME_FMT
+
+#ifndef SOPHUS_FMT_LOGGING
+
+#define SOPHUS_FMT_CSTR(description, ...) description
+#define SOPHUS_FMT_STR(description, ...) std::string(description)
+#define SOPHUS_FMT_PRINT(description, ...) std::printf("%s\n", description)
+
+#else  // SOPHUS_FMT_LOGGING
+
 #ifdef __linux__
 #define SOPHUS_COMPILE_TIME_FMT
 #endif
@@ -37,11 +45,23 @@
 
 #ifdef SOPHUS_COMPILE_TIME_FMT
 // compile-time format check on x
-#define SOPHUS_FMT(x) FMT_STRING(x)
-#else
+#define SOPHUS_FMT_STRING(x) FMT_STRING(x)
+#else  // ! SOPHUS_COMPILE_TIME_FMT
 // identity, hence no compile-time check on x
-#define SOPHUS_FMT(x) x
-#endif
+#define SOPHUS_FMT_STRING(x) x
+#endif  // ! SOPHUS_COMPILE_TIME_FMT
+
+#define SOPHUS_FMT_CSTR(description, ...) \
+  fmt::format(SOPHUS_FMT_STRING(description), ##__VA_ARGS__).c_str()
+
+#define SOPHUS_FMT_STR(description, ...) \
+  fmt::format(SOPHUS_FMT_STRING(description), ##__VA_ARGS__)
+
+#define SOPHUS_FMT_PRINT(description, ...)                   \
+  fmt::print(SOPHUS_FMT_STRING(description), ##__VA_ARGS__); \
+  fmt::print("\n")
+
+#endif  // SOPHUS_FMT_LOGGING
 
 // following boost's assert.hpp
 #undef SOPHUS_ENSURE
@@ -83,11 +103,10 @@ void ensureFailed(char const* function, char const* file, int line,
                   char const* description);
 }
 
-#define SOPHUS_ENSURE(expr, description, ...)        \
-  ((expr) ? ((void)0)                                \
-          : ::Sophus::ensureFailed(                  \
-                SOPHUS_FUNCTION, __FILE__, __LINE__, \
-                fmt::format(SOPHUS_FMT(description), ##__VA_ARGS__).c_str()))
+#define SOPHUS_ENSURE(expr, description, ...)                           \
+  ((expr) ? ((void)0)                                                   \
+          : ::Sophus::ensureFailed(SOPHUS_FUNCTION, __FILE__, __LINE__, \
+                                   SOPHUS_FMT(description)))
 #else
 
 #define SOPHUS_DEDAULT_ENSURE_FAILURE_IMPL(function, file, line, description, \
@@ -97,8 +116,7 @@ void ensureFailed(char const* function, char const* file, int line,
         "Sophus ensure failed in function '%s', "                             \
         "file '%s', line %d.\n",                                              \
         function, file, line);                                                \
-    std::cout << fmt::format(SOPHUS_FMT(description), ##__VA_ARGS__)          \
-              << std::endl;                                                   \
+    SOPHUS_FMT_PRINT(description, ##__VA_ARGS__);                             \
     std::abort();                                                             \
   } while (false)
 

sophus/test_macros.hpp
@@ -1,6 +1,8 @@
 #ifndef SOPUHS_TESTS_MACROS_HPP
 #define SOPUHS_TESTS_MACROS_HPP
 
+#include <sstream>
+
 #include <sophus/types.hpp>
 
 namespace Sophus {
@@ -9,14 +11,20 @@ namespace details {
 template <class Scalar, class Enable = void>
 class Pretty {
  public:
-  static std::string impl(Scalar s) { return fmt::format("{}", s); }
+  static std::string impl(Scalar s) {
+    std::stringstream sstr;
+    sstr << s;
+    return sstr.str();
+  }
 };
 
 template <class Ptr>
 class Pretty<Ptr, enable_if_t<std::is_pointer<Ptr>::value>> {
  public:
   static std::string impl(Ptr ptr) {
-    return fmt::format("{}", std::intptr_t(ptr));
+    std::stringstream sstr;
+    sstr << std::intptr_t(ptr);
+    return sstr.str();
   }
 };
 
@@ -24,7 +32,9 @@ template <class Scalar, int M, int N>
 class Pretty<Eigen::Matrix<Scalar, M, N>, void> {
  public:
   static std::string impl(Matrix<Scalar, M, N> const& v) {
-    return fmt::format("\n{}\n", v);
+    std::stringstream sstr;
+    sstr << "\n" << v << "\n";
+    return sstr.str();
   }
 };
 
@@ -36,18 +46,13 @@ std::string pretty(T const& v) {
 template <class... Args>
 void testFailed(bool& passed, char const* func, char const* file, int line,
                 std::string const& msg) {
-  std::cerr << fmt::format("Test failed in function {}, file {}, line {}\n",
-                           func, file, line);
+  std::fprintf(stderr,
+               "Test failed in function '%s', "
+               "file '%s', line %d.\n",
+               func, file, line);
   std::cerr << msg << "\n\n";
   passed = false;
 }
-
-template <class... Args>
-std::string format(char const* text, Args&&... args) {
-  return fmt::format(text, std::forward<Args>(args)...);
-}
-
-inline std::string format() { return std::string(); }
 
 }  // namespace details
 
@@ -66,12 +71,12 @@ void processTestResult(bool passed) {
 
 /// Tests whether condition is true.
 /// The in-out parameter passed will be set to false if test fails.
-#define SOPHUS_TEST(passed, condition, ...)                                    \
+#define SOPHUS_TEST(passed, condition, descr, ...)                             \
   do {                                                                         \
     if (!(condition)) {                                                        \
-      std::string msg = Sophus::details::format("condition ``{}`` is false\n", \
-                                                SOPHUS_STRINGIFY(condition));  \
-      msg += Sophus::details::format(__VA_ARGS__);                             \
+      std::string msg = SOPHUS_FMT_STR("condition ``{}`` is false\n",          \
+                                       SOPHUS_STRINGIFY(condition));           \
+      msg += SOPHUS_FMT_STR(descr, ##__VA_ARGS__);                             \
       Sophus::details::testFailed(passed, SOPHUS_FUNCTION, __FILE__, __LINE__, \
                                   msg);                                        \
     }                                                                          \
@@ -79,14 +84,14 @@ void processTestResult(bool passed) {
 
 /// Tests whether left is equal to right given a threshold.
 /// The in-out parameter passed will be set to false if test fails.
-#define SOPHUS_TEST_EQUAL(passed, left, right, ...)                            \
+#define SOPHUS_TEST_EQUAL(passed, left, right, descr, ...)                     \
   do {                                                                         \
     if (left != right) {                                                       \
-      std::string msg = Sophus::details::format(                               \
+      std::string msg = SOPHUS_FMT_STR(                                        \
           "{} (={}) is not equal to {} (={})\n", SOPHUS_STRINGIFY(left),       \
           Sophus::details::pretty(left), SOPHUS_STRINGIFY(right),              \
           Sophus::details::pretty(right));                                     \
-      msg += Sophus::details::format(__VA_ARGS__);                             \
+      msg += SOPHUS_FMT_STR(descr, ##__VA_ARGS__);                             \
       Sophus::details::testFailed(passed, SOPHUS_FUNCTION, __FILE__, __LINE__, \
                                   msg);                                        \
     }                                                                          \
@@ -94,14 +99,14 @@ void processTestResult(bool passed) {
 
 /// Tests whether left is equal to right given a threshold.
 /// The in-out parameter passed will be set to false if test fails.
-#define SOPHUS_TEST_NEQ(passed, left, right, ...)                              \
+#define SOPHUS_TEST_NEQ(passed, left, right, descr, ...)                       \
   do {                                                                         \
     if (left == right) {                                                       \
-      std::string msg = Sophus::details::format(                               \
+      std::string msg = SOPHUS_FMT_STR(                                        \
           "{} (={}) should not be equal to {} (={})\n",                        \
           SOPHUS_STRINGIFY(left), Sophus::details::pretty(left),               \
           SOPHUS_STRINGIFY(right), Sophus::details::pretty(right));            \
-      msg += Sophus::details::format(__VA_ARGS__);                             \
+      msg += SOPHUS_FMT_STR(descr, ##__VA_ARGS__);                             \
       Sophus::details::testFailed(passed, SOPHUS_FUNCTION, __FILE__, __LINE__, \
                                   msg);                                        \
     }                                                                          \
@@ -109,16 +114,16 @@ void processTestResult(bool passed) {
 
 /// Tests whether left is approximately equal to right given a threshold.
 /// The in-out parameter passed will be set to false if test fails.
-#define SOPHUS_TEST_APPROX(passed, left, right, thr, ...)                      \
+#define SOPHUS_TEST_APPROX(passed, left, right, thr, descr, ...)               \
   do {                                                                         \
     auto nrm = Sophus::maxMetric((left), (right));                             \
     if (!(nrm < (thr))) {                                                      \
-      std::string msg = Sophus::details::format(                               \
+      std::string msg = SOPHUS_FMT_STR(                                        \
           "{} (={}) is not approx {} (={}); {} is {}; nrm is {}\n",            \
           SOPHUS_STRINGIFY(left), Sophus::details::pretty(left),               \
           SOPHUS_STRINGIFY(right), Sophus::details::pretty(right),             \
           SOPHUS_STRINGIFY(thr), Sophus::details::pretty(thr), nrm);           \
-      msg += Sophus::details::format(__VA_ARGS__);                             \
+      msg += SOPHUS_FMT_STR(descr, ##__VA_ARGS__);                             \
       Sophus::details::testFailed(passed, SOPHUS_FUNCTION, __FILE__, __LINE__, \
                                   msg);                                        \
     }                                                                          \
@@ -126,17 +131,17 @@ void processTestResult(bool passed) {
 
 /// Tests whether left is NOT approximately equal to right given a
 /// threshold. The in-out parameter passed will be set to false if test fails.
-#define SOPHUS_TEST_NOT_APPROX(passed, left, right, thr, ...)                  \
+#define SOPHUS_TEST_NOT_APPROX(passed, left, right, thr, descr, ...)           \
   do {                                                                         \
     auto nrm = Sophus::maxMetric((left), (right));                             \
     if (nrm < (thr)) {                                                         \
-      std::string msg = Sophus::details::format(                               \
+      std::string msg = SOPHUS_FMT_STR(                                        \
           "{} (={}) is approx {} (={}), but it should not!\n {} is {}; nrm "   \
           "is {}\n",                                                           \
           SOPHUS_STRINGIFY(left), Sophus::details::pretty(left),               \
           SOPHUS_STRINGIFY(right), Sophus::details::pretty(right),             \
           SOPHUS_STRINGIFY(thr), Sophus::details::pretty(thr), nrm);           \
-      msg += Sophus::details::format(__VA_ARGS__);                             \
+      msg += SOPHUS_FMT_STR(descr, ##__VA_ARGS__);                             \
       Sophus::details::testFailed(passed, SOPHUS_FUNCTION, __FILE__, __LINE__, \
                                   msg);                                        \
     }                                                                          \

.github/workflows/coverage.yml
@@ -26,7 +26,9 @@ jobs:
         uses: hendrikmuhs/ccache-action@v1
 
       - name: Install dependencies (Linux)
-        run: ./scripts/install_linux_deps.sh
+        run: |
+          ./scripts/install_linux_deps.sh
+          ./scripts/install_linux_fmt_deps.sh
 
       - name: Install Coverage
         run: |

.github/workflows/main.yml
@@ -15,16 +15,25 @@ jobs:
         os: [ubuntu-20.04, macos-10.15]
         build_type: [Debug, Release]
         row_major: [ON, OFF]
+        basic_logging: [ON, OFF]
         exclude:
           - os: macos-10.15
             row_major: ON
+          - os: macos-10.15
+            basic_logging: ON
           - build_type: Release
             row_major: ON
+          - build_type: Release
+            basic_logging: ON
+          - build_type: Debug
+            row_major: ON
+            basic_logging: ON
       fail-fast: false
     env:
       BUILD_TYPE: ${{ matrix.build_type }}
       ROW_MAJOR_DEFAULT: ${{ matrix.row_major }}
-      
+      USE_BASIC_LOGGING: ${{ matrix.basic_logging }}
+
 
     steps:
       - uses: actions/checkout@v2
@@ -44,6 +53,10 @@ jobs:
       - name: Install dependencies (Linux)
         run: ./scripts/install_linux_deps.sh
         if: matrix.os == 'ubuntu-20.04'
+
+      - name: Install fmt dependency (Linux)
+        run: ./scripts/install_linux_fmt_deps.sh
+        if: matrix.os == 'ubuntu-20.04' && matrix.basic_logging == 'OFF'
 
       - name: Install dependencies (Mac OS)
         run: ./scripts/install_osx_deps.sh

CMakeLists.txt
@@ -34,7 +34,12 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake_modules")
 # Find Eigen 3 (dependency)
 find_package(Eigen3 3.3.0 REQUIRED)
 
-find_package(fmt REQUIRED)
+# Basic logging (in ensure and test macros)
+option(USE_BASIC_LOGGING "Use basic logging" OFF)
+if(NOT USE_BASIC_LOGGING)
+  find_package(fmt REQUIRED)
+  add_definitions("-D SOPHUS_FMT_LOGGING")
+endif()
 
 # Define interface library target
 add_library(sophus INTERFACE)
@@ -72,7 +77,10 @@ endif (MSVC)
 
 # Add Eigen interface dependency, depending on available cmake info
 if(TARGET Eigen3::Eigen)
-  target_link_libraries(sophus INTERFACE Eigen3::Eigen fmt::fmt)
+  target_link_libraries(sophus INTERFACE Eigen3::Eigen)
+  if (NOT USE_BASIC_LOGGING)
+    target_link_libraries(sophus INTERFACE fmt::fmt)
+  endif()
   set(Eigen3_DEPENDENCY "find_dependency (Eigen3 ${Eigen3_VERSION})")
 else(TARGET Eigen3::Eigen)
   target_include_directories (sophus SYSTEM INTERFACE ${EIGEN3_INCLUDE_DIR})

examples/CMakeLists.txt
@@ -3,5 +3,8 @@ SET( EXAMPLE_SOURCES HelloSO3)
 
 FOREACH(example_src ${EXAMPLE_SOURCES})
   ADD_EXECUTABLE( ${example_src} ${example_src}.cpp)
-  TARGET_LINK_LIBRARIES( ${example_src} sophus fmt::fmt)
+  TARGET_LINK_LIBRARIES( ${example_src} sophus)
+  if (NOT USE_BASIC_LOGGING)
+    TARGET_LINK_LIBRARIES( ${example_src} fmt::fmt)
+  endif()
 ENDFOREACH(example_src)

scripts/install_linux_deps.sh
@@ -25,13 +25,3 @@ cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
 make -j8
 sudo make install
 cd ../..
-
-git clone https://github.com/fmtlib/fmt.git
-cd fmt
-git checkout 5.3.0
-mkdir build
-cd build
-cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
-make -j3
-sudo make install
-cd ../..

scripts/install_linux_fmt_deps.sh
@@ -1,0 +1,14 @@
+#!/bin/bash
+
+set -x # echo on
+set -e # exit on error
+
+git clone https://github.com/fmtlib/fmt.git
+cd fmt
+git checkout 5.3.0
+mkdir build
+cd build
+cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
+make -j8
+sudo make install
+cd ../..

commit 6bee0e8186a5808b788ea1527bf71411b17fd659

fixing some bugs (#302)
- fixing some bugs introduced in 8d52cd1317b8b175a42141bd50ced181e28089a1 
- TODO: add test coverage for this code branch

sophus/common.hpp
@@ -103,10 +103,11 @@ void ensureFailed(char const* function, char const* file, int line,
                   char const* description);
 }
 
-#define SOPHUS_ENSURE(expr, description, ...)                           \
-  ((expr) ? ((void)0)                                                   \
-          : ::Sophus::ensureFailed(SOPHUS_FUNCTION, __FILE__, __LINE__, \
-                                   SOPHUS_FMT(description)))
+#define SOPHUS_ENSURE(expr, description, ...)                        \
+  ((expr)                                                            \
+       ? ((void)0)                                                   \
+       : ::Sophus::ensureFailed(SOPHUS_FUNCTION, __FILE__, __LINE__, \
+                                SOPHUS_FMT_CSTR(description, ##__VA_ARGS__)))
 #else
 
 #define SOPHUS_DEDAULT_ENSURE_FAILURE_IMPL(function, file, line, description, \

sophus/test_macros.hpp
@@ -1,6 +1,7 @@
 #ifndef SOPUHS_TESTS_MACROS_HPP
 #define SOPUHS_TESTS_MACROS_HPP
 
+#include <iostream>
 #include <sstream>
 
 #include <sophus/types.hpp>