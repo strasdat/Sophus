2b1f87c591af2d56cf9b6fc6faf3b3f965fc657f
Dmitriy Korchemkin <dmitriy.korchemkin@gmail.com>
2021-05-31 20:51:12 +0300

    Run ci builds with -j2

--- scripts/run_cpp_tests.sh
+++ scripts/run_cpp_tests.sh
@@ -7,5 +7,6 @@ mkdir build
 cd build
 pwd
 cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DUSE_BASIC_LOGGING=$USE_BASIC_LOGGING -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
-make -j4
+# Ubuntu builds via Github actions run on 2-core virtual machines
+make -j2
 make CTEST_OUTPUT_ON_FAILURE=1 test
