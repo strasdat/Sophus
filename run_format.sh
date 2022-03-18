0f7f95733d144a86e9797ae2ade344210a5939d5
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2022-03-18 20:18:58 +0100

    ignore sympy folder in run_format.sh (#343)
    * ignore sympy folder in run_format.sh
    
    The sympy folder contains generated cpp files which we don't want to
    auto-format.
    
    This was apparently missed during the py -> sympy folder rename:
    4da9a55431ea4cd48ec8a68ad996a2801e288e6a
    
    See: https://github.com/strasdat/Sophus/pull/342#issuecomment-1072592484
    
    * run_format.sh: fix header files not being formatted

--- run_format.sh
+++ run_format.sh
@@ -1,1 +1,1 @@
-find . -type d \( -path ./py -o -path ./doxyrest_b -o -path "./*/CMakeFiles/*" \) -prune -o  -iname *.hpp -o -iname *.cpp -print | xargs clang-format -i
+find . -type d \( -path ./sympy -o -path ./doxyrest_b -o -path "./*/CMakeFiles/*" \) -prune -o \( -iname "*.hpp" -o -iname "*.cpp" \) -print | xargs clang-format -i
