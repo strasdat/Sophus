853097849502ab6f37d9697d0772ae8992504c59
Hauke Strasdat <strasdat@gmail.com>
2022-08-01 13:45:31 -0700

    Sim3 constr add, missing SOPHUS_FUNC tags (#369)
    * Sim3 constr add, missing SOPHUS_FUNC tags
    
    Co-authored-by: jslee02
    Co-authored-by: aharmat
    
    * fix, random number won't work for jet types

--- test/core/test_sim3.cpp
+++ test/core/test_sim3.cpp
@@ -227,12 +227,20 @@ class Tests {
     RxSO3Type rxso3 = sim3.rxso3();
 
     SOPHUS_TEST_APPROX(passed, Sim3Type(rxso3, translation).matrix(),
-                       sim3.matrix(), Constants<Scalar>::epsilon(), "");
+                       sim3.matrix(), Constants<Scalar>::epsilon(),
+                       "Sim3(RxSO3, translation)");
     SOPHUS_TEST_APPROX(passed,
                        Sim3Type(rxso3.quaternion(), translation).matrix(),
-                       sim3.matrix(), Constants<Scalar>::epsilon(), "");
+                       sim3.matrix(), Constants<Scalar>::epsilon(),
+                       "Sim3(quaternion, translation)");
+    SOPHUS_TEST_APPROX(
+        passed,
+        Sim3Type(rxso3.scale(), rxso3.quaternion().normalized(), translation)
+            .matrix(),
+        sim3.matrix(), Constants<Scalar>::epsilon(),
+        "Sim3(scale, unit_quaternion, translation)");
     SOPHUS_TEST_APPROX(passed, Sim3Type(sim3.matrix()).matrix(), sim3.matrix(),
-                       Constants<Scalar>::epsilon(), "");
+                       Constants<Scalar>::epsilon(), "Sim3(matrix4x4)");
 
     Scalar scale(1.2);
     sim3.setScale(scale);
