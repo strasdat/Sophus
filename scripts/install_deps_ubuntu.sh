9f872c402b569c02d2eb945b4c82be4a4a4e8b2a
Hauke Strasdat <strasdat@gmail.com>
2022-09-19 13:09:28 -0700

    introduce top-level cpp-dir (#381)

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -5,6 +5,8 @@ set -e # exit on error
 
 cmake --version
 
+cd cpp
+
 sudo apt-get -qq update
 sudo apt-get install gfortran libc++-dev libgtest-dev libgoogle-glog-dev libatlas-base-dev libsuitesparse-dev ccache
 wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
@@ -13,4 +15,4 @@ mkdir -p build-eigen
 cd build-eigen
 cmake ../eigen-3.4.0 -DEIGEN_DEFAULT_TO_ROW_MAJOR=$ROW_MAJOR_DEFAULT
 sudo make install
-cd ..
+cd ../..
