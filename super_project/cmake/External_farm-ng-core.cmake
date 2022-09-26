dd6e490a9844442b2c9a50751a2c704ef182602a
Ethan Rublee <ethan.rublee@gmail.com>
2022-09-26 13:15:23 -0700

    Make super_project directory.

---
+++ super_project/cmake/External_farm-ng-core.cmake
@@ -0,0 +1,9 @@
+ExternalProject_Add(farm-ng-core
+    DEPENDS fmt expected eigen
+    GIT_REPOSITORY  "https://github.com/farm-ng/farm-ng-core.git"
+    GIT_TAG "cmake-super-project"
+    PREFIX ${farm_ng_EXT_PREFIX}
+    CMAKE_ARGS
+    ${farm_ng_DEFAULT_ARGS}
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo
+    )
