0ea8323ab92a64acec56088173263dac280064e9
Ethan Rublee <ethan.rublee@gmail.com>
2022-09-26 13:20:50 -0700

    CI?

--- scripts/run_cpp_tests.sh
+++ scripts/run_cpp_tests.sh
@@ -3,8 +3,8 @@
 set -x # echo on
 set -e # exit on error
 
+cd super_project
 mkdir -p build
 cd build
-cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_SOPHUS_TESTS=ON -DBUILD_FARM_NG_PROTOS=$BUILD_PROTOS ..
 make -j2
-make CTEST_OUTPUT_ON_FAILURE=1 test
+
