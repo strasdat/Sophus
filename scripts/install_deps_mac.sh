16ac86d81a116a2adc5a825ffb77a991deb41f57
strasdat <strasdat@gmail.com>
2022-10-10 21:25:42 -0700

    fixes

--- scripts/install_deps_mac.sh
+++ scripts/install_deps_mac.sh
@@ -3,8 +3,5 @@
 set -x # echo on
 set -e # exit on error
 
-wget https://github.com/macports/macports-base/releases/download/v2.7.1/MacPorts-2.7.1-11-BigSur.pkg
-sudo installer -pkg ./MacPorts-2.7.1-11-BigSur.pkg -target /
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-
 sudo PATH=$PATH port install protobuf3-cpp
+sudo port install google-glog
---
+++ scripts/install_mac_ports.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x # echo on
+set -e # exit on error
+
+wget https://github.com/macports/macports-base/releases/download/v2.7.1/MacPorts-2.7.1-11-BigSur.pkg
+sudo installer -pkg ./MacPorts-2.7.1-11-BigSur.pkg -target /
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
