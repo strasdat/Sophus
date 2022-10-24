2a8b6af259a9e84b568f5b5c089487ef8d46c2a3
Steven Lovegrove <stevenlovegrove@gmail.com>
2022-10-24 14:48:08 -0700

    1. Fix bug in UnitVector::tryFromUnitVector (#405)
    2. Refactor UnitVector3 and Ray3 into dimension agnostic templates
    3. Add some UnitVector tests

--- cpp/sophus/geometry/ray.h
+++ cpp/sophus/geometry/ray.h
@@ -18,71 +18,93 @@
 
 namespace sophus {
 
+// Forward declarations
+template <class TScalar, int kN>
+class UnitVector;
+template <class TScalar, int kN>
+class Ray;
+
+// Convenience typedefs
 template <class TScalar>
-class UnitVector3 {
+using UnitVector3 = UnitVector<TScalar, 3>;
+template <class TScalar>
+using UnitVector2 = UnitVector<TScalar, 2>;
+
+using UnitVector2F64 = UnitVector2<double>;
+using UnitVector3F64 = UnitVector3<double>;
+
+template <class TScalar>
+using Ray2 = Ray<TScalar, 2>;
+template <class TScalar>
+using Ray3 = Ray<TScalar, 3>;
+
+using Ray2F64 = Ray2<double>;
+using Ray3F64 = Ray3<double>;
+
+template <class TScalar, int kN>
+class UnitVector {
  public:
   // Precondition: v must be of unit length.
-  static UnitVector3 fromUnitVector(Eigen::Matrix<TScalar, 3, 1> const& v) {
-    farm_ng::Expected<UnitVector3> e_vec = tryFromUnitVector(v);
+  static UnitVector fromUnitVector(Eigen::Matrix<TScalar, kN, 1> const& v) {
+    farm_ng::Expected<UnitVector> e_vec = tryFromUnitVector(v);
     if (!e_vec.has_value()) {
       FARM_FATAL("{}", e_vec.error());
     }
     return e_vec.value();
   }
 
-  static farm_ng::Expected<UnitVector3> tryFromUnitVector(
-      Eigen::Matrix<TScalar, 3, 1> const& v) {
+  static farm_ng::Expected<UnitVector> tryFromUnitVector(
+      Eigen::Matrix<TScalar, kN, 1> const& v) {
     using std::abs;
-    if ((v.squaredNorm() - TScalar(1.0)) < sophus::kEpsilon<TScalar>) {
+    if (abs(v.squaredNorm() - TScalar(1.0)) > sophus::kEpsilon<TScalar>) {
       return FARM_ERROR(
           "v must be unit length is {}.\n{}", v.squaredNorm(), v.transpose());
     }
-    UnitVector3 unit_vector;
+    UnitVector unit_vector;
     unit_vector.vector_ = v;
     return unit_vector;
   }
 
-  static UnitVector3 fromVectorAndNormalize(
-      Eigen::Matrix<TScalar, 3, 1> const& v) {
+  static UnitVector fromVectorAndNormalize(
+      Eigen::Matrix<TScalar, kN, 1> const& v) {
     return fromUnitVector(v.normalized());
   }
 
-  [[nodiscard]] Eigen::Matrix<TScalar, 3, 1> const& vector() const {
+  [[nodiscard]] Eigen::Matrix<TScalar, kN, 1> const& vector() const {
     return vector_;
   }
 
  private:
-  UnitVector3() {}
+  UnitVector() {}
 
   // Class invariant: v_ is of unit length.
-  Eigen::Matrix<TScalar, 3, 1> vector_;
+  Eigen::Matrix<TScalar, kN, 1> vector_;
 };
 
-template <class TScalar>
-class Ray3 {
+template <class TScalar, int kN>
+class Ray {
  public:
-  Ray3(
-      Eigen::Matrix<TScalar, 3, 1> const& origin,
-      UnitVector3<TScalar> const& direction)
+  Ray(Eigen::Matrix<TScalar, kN, 1> const& origin,
+      UnitVector<TScalar, kN> const& direction)
       : origin_(origin), direction_(direction) {}
 
-  Eigen::Matrix<TScalar, 3, 1> const& origin() const { return origin_; }
-  Eigen::Matrix<TScalar, 3, 1>& origin() { return origin_; }
+  Eigen::Matrix<TScalar, kN, 1> const& origin() const { return origin_; }
+  Eigen::Matrix<TScalar, kN, 1>& origin() { return origin_; }
 
-  UnitVector3<TScalar> const& direction() const { return direction_; }
-  UnitVector3<TScalar>& direction() { return direction_; }
+  UnitVector<TScalar, kN> const& direction() const { return direction_; }
+  UnitVector<TScalar, kN>& direction() { return direction_; }
 
-  Eigen::Matrix<TScalar, 3, 1> pointAt(TScalar lambda) const {
+  Eigen::Matrix<TScalar, kN, 1> pointAt(TScalar lambda) const {
     return this->origin_ + lambda * this->direction_.vector();
   }
 
   struct IntersectionResult {
     double lambda;
-    Eigen::Matrix<TScalar, 3, 1> point;
+    Eigen::Matrix<TScalar, kN, 1> point;
   };
 
   std::optional<IntersectionResult> intersect(
-      Eigen::Hyperplane<TScalar, 3> const& plane) const {
+      Eigen::Hyperplane<TScalar, kN> const& plane) const {
     using std::abs;
     TScalar dot_prod = plane.normal().dot(this->direction_.vector());
     if (abs(dot_prod) < sophus::kEpsilon<TScalar>) {
@@ -95,18 +117,15 @@ class Ray3 {
     return result;
   }
 
-  Eigen::Matrix<TScalar, 3, 1> projection(
-      Eigen::Matrix<TScalar, 3, 1> const& point) const {
+  Eigen::Matrix<TScalar, kN, 1> projection(
+      Eigen::Matrix<TScalar, kN, 1> const& point) const {
     return origin_ +
            direction_.getVector().dot(point - origin_) * direction_.vector();
   }
 
  private:
-  Eigen::Matrix<TScalar, 3, 1> origin_;
-  UnitVector3<TScalar> direction_;
+  Eigen::Matrix<TScalar, kN, 1> origin_;
+  UnitVector<TScalar, kN> direction_;
 };
-
-using UnitVector3F64 = UnitVector3<double>;
-using Ray3F64 = Ray3<double>;
 
 }  // namespace sophus
