557b31c72b6cfb274440b5e8d25008c4ec46537b
Steven Lovegrove <stevenlovegrove@gmail.com>
2022-10-31 16:39:40 -0700

    Copy constructors for Ray and UnitNormal. (#409)
    Convenience typedef for writing generic image code.

--- cpp/sophus/geometry/ray.h
+++ cpp/sophus/geometry/ray.h
@@ -74,6 +74,10 @@ class UnitVector {
     return vector_;
   }
 
+  // Class invariant established from sibling we're copying from
+  UnitVector(UnitVector const&) = default;
+  UnitVector& operator=(UnitVector const&) = default;
+
  private:
   UnitVector() {}
 
@@ -87,6 +91,9 @@ class Ray {
   Ray(Eigen::Matrix<TScalar, kN, 1> const& origin,
       UnitVector<TScalar, kN> const& direction)
       : origin_(origin), direction_(direction) {}
+
+  Ray(Ray const&) = default;
+  Ray& operator=(Ray const&) = default;
 
   Eigen::Matrix<TScalar, kN, 1> const& origin() const { return origin_; }
   Eigen::Matrix<TScalar, kN, 1>& origin() { return origin_; }
