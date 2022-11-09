2e6ad2ba1d7b70bad2aaaa495197a3dd516d8547
Edgar Riba <edgar.riba@gmail.com>
2022-11-09 20:49:52 +0100

    add pypi release workflow (#418)
    putting this on hold and pushing by hand the package to the registry to
    avoid creating manual releases that can conflict python/c++
    
    Co-authored-by: Hauke Strasdat <strasdat@gmail.com>

--- setup.py
+++ setup.py
@@ -1,68 +1,28 @@
-#!/usr/bin/env python3
 from setuptools import setup
-from setuptools import Command
-from setuptools.command.develop import develop
-from setuptools.command.egg_info import egg_info
-from setuptools.command.install import install
-from pathlib import Path
-from farm_ng.package.package import build_package_protos, clean_protos
 
+from farm_ng.package.commands import (
+    CleanFilesCommand,
+    BuildProtosInstall,
+    BuildProtosDevelop,
+    BuildProtosEggInfo,
+)
 
-class BuildProtosCommand(Command):
-    user_options = []  # type: ignore
+PROTO_ROOT: str = "protos"
+PACKAGE_ROOT: str = "py"
 
-    def initialize_options(self):
-        pass
+BuildProtosDevelop.user_options.append(("proto-root=", None, PROTO_ROOT))
+BuildProtosDevelop.user_options.append(("package-root=", None, PACKAGE_ROOT))
 
-    def finalize_options(self):
-        pass
+BuildProtosInstall.user_options.append(("proto-root=", None, PROTO_ROOT))
+BuildProtosInstall.user_options.append(("package-root=", None, PACKAGE_ROOT))
 
-    def run(self):
-        build_package_protos(proto_root=Path("protos"), package_root=Path("py"))
+BuildProtosEggInfo.user_options.append(("proto-root=", None, PROTO_ROOT))
+BuildProtosEggInfo.user_options.append(("package-root=", None, PACKAGE_ROOT))
 
-
-class CleanFilesCommand(Command):
-    user_options = []  # type: ignore
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        clean_protos(package_root=Path("py"))
-
-
-class BuildProtosInstall(install):
-    def run(self):
-        # 1. Build the protobufs
-        BuildProtosCommand.run(self)
-        # 2. Run the installation
-        install.run(self)
-        # 3. Clean the generated protobufs
-        CleanFilesCommand.run(self)
-
-
-class BuildProtosDevelop(develop):
-    def run(self):
-        # 1. Build the protobufs
-        BuildProtosCommand.run(self)
-        # 2. Run the installation
-        develop.run(self)
-
-
-class BuildProtosEggInfo(egg_info):
-    def run(self):
-        # 1. Build the protobufs
-        BuildProtosCommand.run(self)
-        # 2. Run the installation
-        egg_info.run(self)
-
+CleanFilesCommand.user_options.append(("package-root=", None, PACKAGE_ROOT))
 
 setup(
     cmdclass={
-        "build_package_protos": BuildProtosCommand,
         "install": BuildProtosInstall,
         "develop": BuildProtosDevelop,
         "egg_info": BuildProtosEggInfo,
