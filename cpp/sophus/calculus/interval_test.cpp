b00fe95f2f89febcf8b79d45dc19e0d10dc47291
Steven Lovegrove <stevenlovegrove@gmail.com>
2023-01-09 20:31:11 -0800

    WIP: Add simple tests for Interval<>

--- cpp/sophus/calculus/interval_test.cpp
+++ cpp/sophus/calculus/interval_test.cpp
@@ -8,6 +8,56 @@
 
 #include "sophus/calculus/interval.h"
 
+#include "sophus/image/image.h"
+
 #include <gtest/gtest.h>
 
 using namespace sophus;
+
+template <typename T>
+void typeChecks() {
+  // Check default construction
+  Interval<T> mm;
+  CHECK(mm.min() == std::numeric_limits<T>::max());
+  CHECK(mm.max() == std::numeric_limits<T>::lowest());
+
+  mm = Interval<T>(10, 22);
+  CHECK(mm == Interval<T>(10, 22));
+  mm.extend(11);
+  CHECK(mm == Interval<T>(10, 22));
+  mm.extend(190);
+  CHECK(mm == Interval<T>(10, 190));
+  mm.extend(7);
+  CHECK(mm == Interval<T>(7, 190));
+
+  mm.extend(Interval<T>(70, 200));
+  CHECK(mm == Interval<T>(7, 200));
+
+  mm.extend(Interval<T>(6, 201));
+  CHECK(mm == Interval<T>(6, 201));
+
+  CHECK(mm != Interval<T>(5, 201));
+  CHECK(mm != Interval<T>(6, 200));
+}
+
+TEST(Interval, scalar_test) {
+  typeChecks<double>();
+  typeChecks<float>();
+  typeChecks<int16_t>();
+  typeChecks<uint8_t>();
+  typeChecks<size_t>();
+  Interval<double> mm(-1.0);
+}
+
+TEST(Interval, image_reduction) {
+  {
+    sophus::MutImage<double> img({10, 10});
+    for (int y = 0; y < img.height(); ++y) {
+      for (int x = 0; x < img.height(); ++x) {
+        img.uncheckedMut(x, y) = x + y * img.width();
+      }
+    }
+    auto min_max = finiteMinMax(img);
+    CHECK(min_max == Interval<double>(0.0, img.width() * img.height() - 1.0));
+  }
+}
