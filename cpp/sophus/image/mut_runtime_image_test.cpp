36a05cbc2b2ae834bb7b8a7273dc4223b4369f7e
Hauke Strasdat <strasdat@gmail.com>
2023-01-21 19:16:21 -0800

    feat: add MutRuntimeImageView and MutRuntimeImage

---
+++ cpp/sophus/image/mut_runtime_image_test.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2011, Hauke Strasdat
+// Copyright (c) 2012, Steven Lovegrove
+// Copyright (c) 2021, farm-ng, inc.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#include "sophus/image/mut_runtime_image.h"
+
+#include <gtest/gtest.h>
+
+using namespace sophus;
+
+#define SOPHUS_TEST_IMG_EQ(lhs, rhs) /* NOLINT*/        \
+  do {                                                  \
+    SOPHUS_ASSERT_EQ(lhs.imageSize(), rhs.imageSize()); \
+    for (int v = 0; v < lhs.imageSize().height; ++v) {  \
+      for (int u = 0; u < lhs.imageSize().width; ++u) { \
+        SOPHUS_ASSERT_EQ(lhs(u, v), rhs(u, v));         \
+      }                                                 \
+    }                                                   \
+  } while (false)
---
+++ cpp/sophus/image/mut_runtime_image_view.h
@@ -0,0 +1,91 @@
+// Copyright (c) 2011, Hauke Strasdat
+// Copyright (c) 2012, Steven Lovegrove
+// Copyright (c) 2021, farm-ng, inc.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#pragma once
+
+#include "sophus/image/runtime_image_view.h"
+
+#include <variant>
+
+namespace sophus {
+
+template <class TPredicate = AnyImagePredicate>
+class MutRuntimeImageView : public RuntimeImageView<TPredicate> {
+ public:
+  /// Create type-erased image view from ImageView.
+  ///
+  /// By design not "explicit".
+  template <class TPixel>
+  MutRuntimeImageView(MutImageView<TPixel> const& image)
+      : MutRuntimeImageView(
+            image.shape(),
+            RuntimePixelType::fromTemplate<TPixel>(),
+            image.ptr()) {
+    static_assert(TPredicate::template isTypeValid<TPixel>());
+  }
+
+  MutRuntimeImageView(
+      ImageShape const& image_shape,
+      RuntimePixelType const& pixel_type,
+      void const* ptr)
+      : RuntimeImageView<TPredicate>(image_shape, pixel_type, ptr) {}
+
+  /// Return true is this contains data of type TPixel.
+  template <class TPixel>
+  [[nodiscard]] bool has() const noexcept {
+    RuntimePixelType expected_type = RuntimePixelType::fromTemplate<TPixel>();
+    return expected_type == this->pixel_type_;
+  }
+
+  /// Returns v-th row pointer.
+  ///
+  /// Precondition: v must be in [0, height).
+  [[nodiscard]] uint8_t* rawMutRowPtr(int v) {
+    return ((uint8_t*)(rawMutPtr()) + v * this->shape_.pitchBytes());
+  }
+
+  [[nodiscard]] uint8_t* rawMutPtr() { return this->ptr_; }
+
+  /// Returns subview with shared ownership semantics of whole image.
+  [[nodiscard]] MutRuntimeImageView mutSubview(
+      Eigen::Vector2i uv, sophus::ImageSize size) const {
+    SOPHUS_ASSERT(this->imageSize().contains(uv));
+    SOPHUS_ASSERT_LE(uv.x() + size.width, this->shape_.width());
+    SOPHUS_ASSERT_LE(uv.y() + size.height, this->shape_.height());
+
+    auto const shape =
+        ImageShape::makeFromSizeAndPitchUnchecked(size, this->pitchBytes());
+    const size_t row_offset =
+        uv.x() * this->numBytesPerPixelChannel() * this->numChannels();
+    uint8_t* ptr = this->rawMutPtr() + uv.y() * this->pitchBytes() + row_offset;
+    return MutRuntimeImageView{shape, this->pixel_type_, ptr};
+  }
+
+  /// Returns typed image view.
+  ///
+  /// Precondition: this->has<TPixel>()
+  template <class TPixel>
+  [[nodiscard]] MutImageView<TPixel> mutImageView() const noexcept {
+    if (!this->has<TPixel>()) {
+      RuntimePixelType expected_type = RuntimePixelType::fromTemplate<TPixel>();
+
+      SOPHUS_PANIC(
+          "expected type: {}\n"
+          "actual type: {}",
+          expected_type,
+          this->pixel_type_);
+    }
+
+    return MutImageView<TPixel>(
+        this->shape_, reinterpret_cast<TPixel const*>(this->ptr_));
+  }
+
+ protected:
+  MutRuntimeImageView() = default;
+};
+}  // namespace sophus
