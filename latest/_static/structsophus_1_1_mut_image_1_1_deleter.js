b3d2a94de6ca0ad1237bd06c1fd9991c669b8683
strasdat <strasdat@users.noreply.github.com>
2023-01-23 21:10:27 +0000

    deploy: d551c3fa147e7bb55fca96ff044b00794fce6aa4

--- latest/_static/structsophus_1_1_mut_image_1_1_deleter.js
+++ latest/_static/structsophus_1_1_mut_image_1_1_deleter.js
@@ -2,6 +2,6 @@ var structsophus_1_1_mut_image_1_1_deleter =
 [
     [ "Deleter", "structsophus_1_1_mut_image_1_1_deleter.html#a25a03a4763f27295c409f280f76ecb5d", null ],
     [ "Deleter", "structsophus_1_1_mut_image_1_1_deleter.html#a1c4c9f43ec7b35e4bafc4fa21780ff79", null ],
-    [ "operator()", "structsophus_1_1_mut_image_1_1_deleter.html#a081b0d05d4060d4e54d68c8135fd4eab", null ],
+    [ "operator()", "structsophus_1_1_mut_image_1_1_deleter.html#ad9a1f15f620ebc6e0694a0422a5a0dec", null ],
     [ "image_deleter", "structsophus_1_1_mut_image_1_1_deleter.html#a1abc371d3f2cbf361724655c02a985e2", null ]
 ];
