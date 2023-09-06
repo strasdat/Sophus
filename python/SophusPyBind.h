fdb80d6e611917385b163345690541767ae5f062
Cheng Peng <chpeng@meta.com>
2023-09-06 10:52:41 -0700

    update SophusPyBind

--- python/SophusPyBind.h
+++ python/SophusPyBind.h
@@ -14,12 +14,37 @@
  * limitations under the License.
  */
 
-#include "SophusPyBind.h"
+#pragma once
 
-namespace py = pybind11;
-using namespace sophus;
+#include "SE3PyBind.h"
+#include "SO3PyBind.h"
 
-PYBIND11_MODULE(sophus, m) {
-  py::module sophus = m.def_submodule("pysophus");
-  sophus::exportSophus(sophus);
+#include <sstream>
+// By default, Sophus calls std::abort when a pre-condition fails. Register a handler that raises
+// an exception so we don't crash the Python process.
+#ifdef SOPHUS_DISABLE_ENSURES
+#undef SOPHUS_DISABLE_ENSURES
+#endif
+#ifndef SOPHUS_ENABLE_ENSURE_HANDLER
+#define SOPHUS_ENABLE_ENSURE_HANDLER
+#endif
+
+namespace sophus {
+inline void
+ensureFailed(char const* function, char const* file, int line, char const* description) {
+  std::stringstream message;
+  message << "'SOPHUS_ENSURE' failed in function '" << function << "', on line '" << line
+          << "' of file '" << file << "'. Full description:" << std::endl
+          << description;
+  throw std::domain_error(message.str());
 }
+
+inline void exportSophus(pybind11::module& module) {
+  exportSO3Group<double>(module, "SO3");
+  exportSE3Transformation<double>(module, "SE3");
+
+  exportSE3Average<double>(module);
+  exportSE3Interpolate<double>(module);
+}
+
+} // namespace sophus
