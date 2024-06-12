8a4e7936dece0c8d653993901b7ce4bf4add909f
Hauke Strasdat <strasdat@gmail.com>
2024-06-12 01:17:10 -0700

    compiler toolchain tweaks

--- scripts/run_cpp_tests_gcc.sh
+++ scripts/run_cpp_tests_gcc.sh
@@ -6,7 +6,7 @@ set -e # exit on error
 mkdir build
 cd build
 pwd
-cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
+cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_COMPILE_WARNING_AS_ERROR=On -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
 # Ubuntu builds via Github actions run on 2-core virtual machines
 make -j2
 ctest --output-on-failure
